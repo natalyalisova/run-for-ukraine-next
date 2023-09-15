306236502a370883016e1a9e428bbff2ccdee79e
Nataliia Lisova <nglisova@gmail.com>
2023-09-15 22:05:05 +0300

    start

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const {i18n} = require('./next-i18next.config')
+
+const nextConfig = {
+    i18n:{
+        locales:["en", "us"],
+        defaultLocale: "en",
+    },
+    reactStrictMode: true,
+}
 
 module.exports = nextConfig
