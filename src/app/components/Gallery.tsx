019585a614c74f1289c70db969337473711fbc76
Nataliia Lisova <nglisova@gmail.com>
2023-11-09 15:16:40 +0200

    Photogallery (#4)

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -11,27 +11,47 @@ const Gallery = () => {
       <div className="mt-4 h-1 w-24 bg-yellow-gold mx-auto mb-3"></div>
 
       <div className="gallery-container flex flex-wrap gap-3 p-5 justify-center ">
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_20220529ukrainiansatottawarw_ta271.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_120220529ukrainiansatottawarw_ta312.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_120220529ukrainiansatottawarw_ta317.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_20220529ukrainiansatottawarw_ta349.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_a7408348.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_a7408350.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_1dsc_3522.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_1cm2_8662.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_a7408354-edit.jpg" />
-        <img
-          className="flex self-start"
-          src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_dsc_3502-long.jpg"
-        />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_1ava_3468-long.jpg" />
-        <img
-          className="flex self-start"
-          src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_1dsc_3511-long.jpg"
-        />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_dsc_3547.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_dsc_3534.jpg" />
-        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_120220529ukrainiansatottawarw_ta312.jpg" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1565.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1568.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1544.JPG" />
+
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_4471.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_4517.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_4475.JPG" />
+
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_4397.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1529.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1530.JPG" />
+
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1558.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_4481.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1507.JPG" />
+
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1503.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_2462.JPG" />
+        <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_7535.JPG" />
+
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_20220529ukrainiansatottawarw_ta271.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_120220529ukrainiansatottawarw_ta312.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_120220529ukrainiansatottawarw_ta317.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_20220529ukrainiansatottawarw_ta349.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_a7408348.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_a7408350.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_1dsc_3522.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_1cm2_8662.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_a7408354-edit.jpg" />*/}
+        {/*<img*/}
+        {/*  className="flex self-start"*/}
+        {/*  src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_dsc_3502-long.jpg"*/}
+        {/*/>*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_1ava_3468-long.jpg" />*/}
+        {/*<img*/}
+        {/*  className="flex self-start"*/}
+        {/*  src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_1dsc_3511-long.jpg"*/}
+        {/*/>*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_dsc_3547.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_dsc_3534.jpg" />*/}
+        {/*<img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_120220529ukrainiansatottawarw_ta312.jpg" />*/}
         <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_img_2707.jpg" />
         <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_img_2706.jpg" />
         <img src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2Fgallery%2Frsz_img_2708.jpg" />
--- src/app/components/Gather.tsx
+++ src/app/components/Gather.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next-intl/link";
 import TextGradient from "@/app/components/AnimatedTextGradient";
 import React from "react";
@@ -7,107 +6,111 @@ import About from "@/app/components/About";
 import DonateWidget from "./donate/DonateWidget";
 import { campaignCode } from "../constants";
 import { auto } from "@popperjs/core";
+import Team from "@/app/components/Team";
 
 const Gather = () => {
   const t = useTranslations("Gather");
 
   return (
-    <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-2 justify-center p-6">
-      <div className="flexCenter mb-6">
-        <img
-          src="/images/walkie-talkie.png"
-          alt={"walkie-talkie image"}
-          width={300}
-          height={auto}
-        />
-      </div>
-      <div className="flex justify-center flex-col" id="gather">
-        <h2 className="title">{t("fundraising-goal-title")}</h2>
-        <p className="inline text-bace md:text-lg">
-          {t("fundraising-goal-description-1")}
-          <span className="text-strong-azure text-bace md:text-lg underline underline-offset-2">
-            <Link
-              href={
-                "https://www.instagram.com/p/CpDcpkMud_l/?igshid=YmMyMTA2M2Y%3D"
-              }
-            >
-              &quot;{t("second-front-name")}&quot;
-            </Link>
-          </span>
-          {t("fundraising-goal-description-2")}
-        </p>
-      </div>
-      <div className="flex justify-center flex-col mb-1">
-        <p className="text-base md:text-lg">
-          <span className="font-semibold">
-            &quot;{t("second-front-foundation-title")}&quot;
-          </span>
-          {t("second-front-foundation-description-1")}
-        </p>
-        <p className="font-semibold text-base md:text-lg">
-          {t("second-front-foundation-description-2")}
-        </p>
-      </div>
-      <div
-        className="flex justify-center flex-col text-2xl text-center w-[340px] md:w-[360px] mx-auto"
-        id="donate"
-      >
-        <DonateWidget
-          campaign={campaignCode}
-          showCollections
-          targetCollections={69920}
-          goalTranslation={t("goal")}
-          collectDateTranslation={t("Collect-to-date")}
-          raisedTranslation={t("raised")}
-          thankYouTranslation={t("thank - you")}
-          anotherContributionTranslation={t("another-contribution")}
-          processingTranslation={t("processing")}
-          yourDonationTranslation={t("your-donation")}
-          anotherWindowTranslation={t("another-window")}
-          cancelTranslation={t("cancel")}
-          selectAmountTranslation={t("select-amount")}
-          includeEmailTranslation={t("include-email")}
-          enterAmountTranslation={t("enter-amount")}
-          emailOptionalTranslation={t("email-optional")}
-          fullNameOptionalTranslation={t("full-name-optional")}
-          addNoteTranslation={t("add-note")}
-          registerTranslation={t("register")}
-          donateTranslation={t("donate")}
-        ></DonateWidget>
-      </div>
-
-      <div className="flexCenter my-6">
-        <img
-          src="/images/amelina-olga.png"
-          alt={"Amelina Olga image"}
-          width={500}
-          height={auto}
-        />
-      </div>
-
-      <About />
-
-      <div className="flexCenter my-6 text-2xl font-bold">
-        <TextGradient
-          text={t("RUN - EAT - DONATE - REPEAT!")}
-          fontStyle={"text-3xl"}
-        />
-      </div>
-      <div className="flexCenter my-6 hero-video">
-        <div className="relative">
-          <video
-            autoPlay
-            loop
-            playsInline
-            muted
-            src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2FBD995476-9DBA-4300-BA04-7775CBD40D60.MOV"
-          ></video>
-          <p className="absolute  bg-black opacity-60 font-bold text-4xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            #RunForUkraine
+    <>
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-2 justify-center p-6">
+        <div className="flexCenter mb-6">
+          <img
+            src="/images/walkie-talkie.png"
+            alt={"walkie-talkie image"}
+            width={300}
+            height={auto}
+          />
+        </div>
+        <div className="flex justify-center flex-col" id="gather">
+          <h2 className="title">{t("fundraising-goal-title")}</h2>
+          <p className="inline text-bace md:text-lg">
+            {t("fundraising-goal-description-1")}
+            <span className="text-strong-azure text-bace md:text-lg underline underline-offset-2">
+              <Link
+                href={
+                  "https://www.instagram.com/p/CpDcpkMud_l/?igshid=YmMyMTA2M2Y%3D"
+                }
+              >
+                &quot;{t("second-front-name")}&quot;
+              </Link>
+            </span>
+            {t("fundraising-goal-description-2")}
           </p>
         </div>
+        <div className="flex justify-center flex-col mb-1">
+          <p className="text-base md:text-lg">
+            <span className="font-semibold">
+              &quot;{t("second-front-foundation-title")}&quot;
+            </span>
+            {t("second-front-foundation-description-1")}
+          </p>
+          <p className="font-semibold text-base md:text-lg">
+            {t("second-front-foundation-description-2")}
+          </p>
+        </div>
+        <div
+          className="flex justify-center flex-col text-2xl text-center w-[340px] md:w-[360px] mx-auto"
+          id="donate"
+        >
+          <DonateWidget
+            campaign={campaignCode}
+            showCollections
+            targetCollections={69920}
+            goalTranslation={t("goal")}
+            collectDateTranslation={t("Collect-to-date")}
+            raisedTranslation={t("raised")}
+            thankYouTranslation={t("thank - you")}
+            anotherContributionTranslation={t("another-contribution")}
+            processingTranslation={t("processing")}
+            yourDonationTranslation={t("your-donation")}
+            anotherWindowTranslation={t("another-window")}
+            cancelTranslation={t("cancel")}
+            selectAmountTranslation={t("select-amount")}
+            includeEmailTranslation={t("include-email")}
+            enterAmountTranslation={t("enter-amount")}
+            emailOptionalTranslation={t("email-optional")}
+            fullNameOptionalTranslation={t("full-name-optional")}
+            addNoteTranslation={t("add-note")}
+            registerTranslation={t("register")}
+            donateTranslation={t("donate")}
+          ></DonateWidget>
+        </div>
+
+        <div className="flexCenter my-6">
+          <img
+            src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/IMG_1568.JPG"
+            alt={"Amelina Olga image"}
+            width={500}
+            height={auto}
+          />
+        </div>
+
+        <About />
+
+        <div className="flexCenter my-6 text-2xl font-bold">
+          <TextGradient
+            text={t("RUN - EAT - DONATE - REPEAT!")}
+            fontStyle={"text-3xl"}
+          />
+        </div>
+        <div className="flexCenter my-6 hero-video">
+          <div className="relative">
+            <video
+              autoPlay
+              loop
+              playsInline
+              muted
+              src="https://secondfront-cdn.nyc3.cdn.digitaloceanspaces.com/runforukraine2023%2FBD995476-9DBA-4300-BA04-7775CBD40D60.MOV"
+            ></video>
+            <p className="absolute  bg-black opacity-60 font-bold text-4xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              #RunForUkraine
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
+      <Team />
+    </>
   );
 };
 
