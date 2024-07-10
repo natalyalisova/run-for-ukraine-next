9fb541217291f4a7a247445a299bd047030b9502
Nataliia Lisova <nglisova@gmail.com>
2024-07-10 16:35:50 +0300

    Registration independence day form UI(#8)

---
+++ src/app/[locale]/independence/page.jsx
@@ -0,0 +1,12 @@
+import { RegistrationForRunForm } from "../../components/RegistrationForRunForm";
+import { UnderConstraction } from "../../components/UnderConstraction";
+import React from "react";
+
+export default function RegistrationForRun() {
+  return (
+    <>
+      <UnderConstraction />
+      <RegistrationForRunForm />
+    </>
+  );
+}
