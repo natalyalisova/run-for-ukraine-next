4e048111b1d4e702fe89b7e2c5a294b99f5cd1f8
Nataliia Lisova <nglisova@gmail.com>
2024-08-06 13:11:42 +0300

    feature: text updates to registration in TA

--- src/app/components/RegistrationForRunTLVForm.tsx
+++ src/app/components/RegistrationForRunTLVForm.tsx
@@ -8,8 +8,13 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faChevronLeft,
+  faCircleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/navigation";
+import TermsNConditions from "@/app/components/TermsNConditions";
 
 const RegistrationForRunTLVForm = () => {
   const [email, setEmail] = useState<string>("");
@@ -68,13 +73,53 @@ const RegistrationForRunTLVForm = () => {
       <div className="my-12 text-center max-w-screen-md">
         <TextGradient text={"Registration for a race in Tel Aviv"} />
         <div className="mt-4 h-1 w-64 bg-yellow-gold mx-auto mb-6"></div>
-        <div className="md:mx-auto">
-          <p className="text-small md:text-base">
-            This race dedicated to the 33rd anniversary of Ukraine's
-            independence
+        <div className="max-w-3xl text-start md:mx-auto text-small md:text-lg">
+          <p className="mt-2">
+            <FontAwesomeIcon
+              icon={faCheck}
+              style={{ color: "#0057b8" }}
+              className="mr-4"
+            />
+            After registration, within a few days, you will receive a
+            confirmation letter to the e-mail address you specified.
           </p>
-          <form onSubmit={handleRegister} className="mt-12 mx-auto md:w-96">
-            <div>
+          <p className="mt-2">
+            <FontAwesomeIcon
+              icon={faCheck}
+              style={{ color: "#0057b8" }}
+              className="mr-4"
+            />
+            The starting number will be available from 6:15 to 7:00 at the start
+            of the run.
+          </p>
+          <p className="mt-2">
+            <FontAwesomeIcon
+              icon={faCheck}
+              style={{ color: "#0057b8" }}
+              className="mr-4"
+            />
+            Wear the legendary #RunForUkraine t-shirts (you can also buy them at
+            the start).
+          </p>
+          <p className="mt-2">
+            <FontAwesomeIcon
+              icon={faCheck}
+              style={{ color: "#0057b8" }}
+              className="mr-4"
+            />
+            Take the flags with you 🇺🇦, put on the 💛💙 uniform.
+          </p>
+          <p className="mt-2">
+            <FontAwesomeIcon
+              icon={faCheck}
+              style={{ color: "#0057b8" }}
+              className="mr-4"
+            />
+            Each participant at the finish line will receive our branded pin
+            #RunForUkraine
+          </p>
+          <form onSubmit={handleRegister} className="mt-12  max-w-3xl">
+            <div className="md:w-96">
               <input
                 type="text"
                 value={name}
@@ -84,8 +129,6 @@ const RegistrationForRunTLVForm = () => {
                 onChange={(e) => setName(e.target.value)}
                 required
               />
-            </div>
-            <div>
               <input
                 type="email"
                 value={email}
@@ -109,12 +152,12 @@ const RegistrationForRunTLVForm = () => {
                 </Select>
               </FormControl>
             </div>
-            <div>
-              <p className="text-start">
-                One Israeli Shekel (ILS) is approximately equivalent to 11
-                Ukrainian Hryvnias (UAH)
-              </p>
 
+            <p className="text-start">
+              One Israeli Shekel (ILS) is approximately equivalent to 11
+              Ukrainian Hryvnias (UAH)
+            </p>
+            <div className="md:w-96">
               <Link
                 href={
                   "https://www.xe.com/currencyconverter/convert/?Amount=1&From=ILS&To=UAH"
@@ -144,69 +187,32 @@ const RegistrationForRunTLVForm = () => {
                 </Select>
               </FormControl>
             </div>
-            <div className="inline-flex items-start">
-              <label
-                className="relative flex items-center mt-1 p-2 rounded-full cursor-pointer"
-                htmlFor="check"
-              >
-                <input
-                  type="checkbox"
-                  className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
-                  id="check"
-                  onChange={() => setIsChecked(!isChecked)}
-                />
-                <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-3.5 w-3.5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    stroke="currentColor"
-                    stroke-width="1"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </span>
-              </label>
-              <p className="mt-1 font-light text-black cursor-pointer select-none text-left pl-2 pt-2 text-small md:text-base">
-                By clicking 'Register' you agree to the Run For Ukraine{" "}
-                <Link
-                  href={"/terms-n-conditions"}
-                  target="_blank"
-                  className="text-gray-700 rounded underline-offset-1 underline md:bg-transparent hover:text-strong-azure mr-2"
-                  aria-current="page"
-                >
-                  Terms and Conditions
-                </Link>
-                and
-                <Link
-                  href={"/cookies-privacy-policy"}
-                  target="_blank"
-                  className=" text-gray-700 underline underline-offset-1 rounded md:bg-transparent hover:text-strong-azure ml-1"
-                  aria-current="page"
-                >
-                  Cookies & Privacy Policy
-                </Link>
-                .
-              </p>
-            </div>
+            <TermsNConditions
+              isChecked={isChecked}
+              setIsChecked={setIsChecked}
+            />
             {error && <p>{error}</p>}
             {message && <p>{message}</p>}
-            <p className="text-start font-bold mt-2">
+            <p className="text-start font-bold mt-2 bg-yellow-gold p-6 rounded-md">
+              <FontAwesomeIcon
+                icon={faCircleExclamation}
+                style={{ color: "#0057b8" }}
+                className="mr-4"
+              />
               Be sure to add your email address in the payment comment in next
               step!
             </p>
-            <button
-              type="submit"
-              disabled={!email || !name || !distance || !donation || !isChecked}
-              className="w-full mt-6 p-3 rounded-md bg-strong-azure text-yellow-gold hover:bg-blue-600 disabled:bg-neutral-400 disabled:text-white focus:outline-none"
-            >
-              Register
-            </button>
+            <div className="md:w-96 mx-auto">
+              <button
+                type="submit"
+                disabled={
+                  !email || !name || !distance || !donation || !isChecked
+                }
+                className="w-full mt-6 p-3 rounded-md bg-strong-azure text-yellow-gold hover:bg-blue-600 disabled:bg-neutral-400 disabled:text-white focus:outline-none"
+              >
+                Register
+              </button>
+            </div>
           </form>
         </div>
       </div>
