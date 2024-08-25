20009a42062912ed0ba1066bef7dccc4b79f670e
Nataliia Lisova <nglisova@gmail.com>
2024-08-25 23:16:50 +0300

    update: after event changes

--- src/app/components/Run2024.tsx
+++ src/app/components/Run2024.tsx
@@ -16,59 +16,75 @@ const Run2024 = () => {
       <div className="flex items-center flex-col">
         <div className="justify-start max-w-[1024px]">
           <h2 className="title text-strong-azure">{t("title")}</h2>
-          <div className="mt-6">
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3-content"
-                id="panel3-header"
-                className="text-xl text-strong-azure font-semibold"
-              >
-                {t("sub-title-1")}
-              </AccordionSummary>
-              <AccordionDetails className="text-start">
-                <p>🗓️ {t("description-1")}</p>
-                <p>📍 {t("description-2")}</p>
-                <p>✅ {t("description-3")} &#8372; 150 (UAH)</p>
-                <p>🏃 {t("description-4")} </p>
-                <p className="mt-3">{t("description-5")}</p>
-              </AccordionDetails>
-              <AccordionActions>
-                <button
-                  onClick={() => router.push("/register-online-2024")}
-                  className="w-full mt-6 p-3 rounded-md text-xl font-semibold bg-strong-azure text-yellow-gold hover:bg-blue-600 focus:outline-none"
-                >
-                  {t("button-1")}
-                </button>
-              </AccordionActions>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3-content"
-                id="panel3-header"
-                className="text-xl text-strong-azure font-semibold"
-              >
-                {t("sub-title-2")}
-              </AccordionSummary>
-              <AccordionDetails className="text-start">
-                <p>🗓️ {t("description-6")} </p>
-                <p>
-                  📍 {t("description-7")} I ❤️ TLV {t("description-10")}
-                </p>
-                <p>⏰ {t("description-11")} Старт: 7:00 AM </p>
-                <p>✅ {t("description-8")} &#8362; 100 (NIS)</p>
-                <p>🏃 {t("description-9")}</p>
-              </AccordionDetails>
-              <AccordionActions>
-                <button
-                  onClick={() => router.push("/register-tlv-2024")}
-                  className="w-full mt-6 p-3 rounded-md bg-strong-azure text-xl font-semibold text-yellow-gold hover:bg-blue-600 focus:outline-none"
-                >
-                  {t("button-2")}
-                </button>
-              </AccordionActions>
-            </Accordion>
+          <div className="mt-6 text-start">
+            <h2 className="text-xl text-strong-azure font-semibold">
+              {" "}
+              {t("sub-title-1")}
+            </h2>
+            <p>🗓️ {t("description-1")}</p>
+            <p>📍 {t("description-2")}</p>
+            <p>✅ {t("description-3")} &#8372; 150 (UAH)</p>
+            <p>🏃 {t("description-4")} </p>
+            <p className="mt-3">{t("description-5")}</p>
+            <button
+              onClick={() => router.push("/register-online-2024")}
+              className="w-full mt-6 p-3 rounded-md text-xl font-semibold bg-strong-azure text-yellow-gold hover:bg-blue-600 focus:outline-none"
+            >
+              {t("button-1")}
+            </button>
+
+            {/*<Accordion>*/}
+            {/*  <AccordionSummary*/}
+            {/*      expandIcon={<ExpandMoreIcon/>}*/}
+            {/*      aria-controls="panel3-content"*/}
+            {/*      id="panel3-header"*/}
+            {/*      className="text-xl text-strong-azure font-semibold"*/}
+            {/*  >*/}
+            {/*    {t("sub-title-1")}*/}
+            {/*  </AccordionSummary>*/}
+            {/*  <AccordionDetails className="text-start">*/}
+            {/*    <p>🗓️ {t("description-1")}</p>*/}
+            {/*    <p>📍 {t("description-2")}</p>*/}
+            {/*    <p>✅ {t("description-3")} &#8372; 150 (UAH)</p>*/}
+            {/*    <p>🏃 {t("description-4")} </p>*/}
+            {/*    <p className="mt-3">{t("description-5")}</p>*/}
+            {/*  </AccordionDetails>*/}
+            {/*  <AccordionActions>*/}
+            {/*    <button*/}
+            {/*        onClick={() => router.push("/register-online-2024")}*/}
+            {/*        className="w-full mt-6 p-3 rounded-md text-xl font-semibold bg-strong-azure text-yellow-gold hover:bg-blue-600 focus:outline-none"*/}
+            {/*    >*/}
+            {/*      {t("button-1")}*/}
+            {/*    </button>*/}
+            {/*  </AccordionActions>*/}
+            {/*</Accordion>*/}
+            {/*<Accordion>*/}
+            {/*  <AccordionSummary*/}
+            {/*      expandIcon={<ExpandMoreIcon/>}*/}
+            {/*      aria-controls="panel3-content"*/}
+            {/*      id="panel3-header"*/}
+            {/*      className="text-xl text-strong-azure font-semibold"*/}
+            {/*  >*/}
+            {/*    {t("sub-title-2")}*/}
+            {/*  </AccordionSummary>*/}
+            {/*  <AccordionDetails className="text-start">*/}
+            {/*    <p>🗓️ {t("description-6")} </p>*/}
+            {/*    <p>*/}
+            {/*      📍 {t("description-7")} I ❤️ TLV {t("description-10")}*/}
+            {/*    </p>*/}
+            {/*    <p>⏰ {t("description-11")} Старт: 7:00 AM </p>*/}
+            {/*    <p>✅ {t("description-8")} &#8362; 100 (NIS)</p>*/}
+            {/*    <p>🏃 {t("description-9")}</p>*/}
+            {/*  </AccordionDetails>*/}
+            {/*  <AccordionActions>*/}
+            {/*    <button*/}
+            {/*        onClick={() => router.push("/register-tlv-2024")}*/}
+            {/*        className="w-full mt-6 p-3 rounded-md bg-strong-azure text-xl font-semibold text-yellow-gold hover:bg-blue-600 focus:outline-none"*/}
+            {/*    >*/}
+            {/*      {t("button-2")}*/}
+            {/*    </button>*/}
+            {/*  </AccordionActions>*/}
+            {/*</Accordion>*/}
           </div>
         </div>
       </div>
