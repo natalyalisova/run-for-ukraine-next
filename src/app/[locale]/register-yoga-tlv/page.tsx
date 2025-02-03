d7c3b93fc01f0052213d74079dad52ec28c30bf4
Nataliia Lisova <nglisova@gmail.com>
2025-02-03 14:56:56 +0200

    update: event updates

--- src/app/[locale]/register-yoga-tlv/page.tsx
+++ src/app/[locale]/register-yoga-tlv/page.tsx
@@ -5,7 +5,12 @@ export default function RegistrationForRun() {
   return (
     <>
       <div className="mt-16 md:mt-[150px]"></div>
-      <RegistrationForYogaTLVForm />
+
+      <main className="flex items-center p-6 flex-col">
+        <p className="font-semibold mb-2">❗️Реєстрацію закрито</p>
+      </main>
+
+      {/*<RegistrationForYogaTLVForm />*/}
     </>
   );
 }
